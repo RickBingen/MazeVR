e9db4ad7b0ba8fedbb004b37226e0f1c5b0378c2
RickBingen <51863040+RickBingen@users.noreply.github.com>
2021-07-04 10:39:10 -0500

    Add files via upload

---
+++ Assets/Scripts/CheckWin.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckWin : MonoBehaviour
+{
+    [SerializeField] GameObject winMessage;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        winMessage.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("MainCamera"))
+        {
+            if(!winMessage.activeInHierarchy)
+                winMessage.SetActive(true);
+        }
+    }
+}
